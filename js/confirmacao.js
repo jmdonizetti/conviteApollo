0f605ea2a8e715c6194d9777f76f5aa86c6d18d4
Joao <j_donizetti@outlook.com>
2024-04-26 22:03:02 -0300

    Retirando numero de celular

--- js/confirmacao.js
+++ js/confirmacao.js
@@ -5,7 +5,7 @@ function submitForm() {
     let message = `Envie a mensagem para confirmar a presença dos convidados: ${names}. Muito Obrigado sua presença é motivo de muita alegria.`;
 
     // Criar a URL do WhatsApp com a mensagem pré-definida
-    let whatsappURL = "https://api.whatsapp.com/send?phone=+5512982268834&text=" + encodeURIComponent(message);
+    let whatsappURL = "https://api.whatsapp.com/send?phone=+#&text=" + encodeURIComponent(message);
 
     // Abrir a URL do WhatsApp em uma nova aba
     window.open(whatsappURL, '_blank');
